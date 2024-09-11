52ccea7bc331b135796866a8b811351710952d21
TontonBanton <pancla13ulas@gmail.com>
2024-09-11 15:18:36 +0800

    NuxtImage Static Optimization

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -5,7 +5,7 @@
   <header class="py-4 border-b">
     <nav class="flex container items-center">
       <NuxtLink to="/" href="" class="flex gap-1 items-center">
-        <img class="w-14" src="/icon-green.png" alt="" />
+        <NuxtImg width="56px" densities="x1" format="webp" src="/icon-green.png" alt="" />
         <span class="text-3xl font-bold">Nuxt Project</span>
       </NuxtLink>
       <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
